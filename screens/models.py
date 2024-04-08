6daa17e0d0e178b3526fbf1bfd9a5d6da2e199b6
badmash417 <56015564+badmash417@users.noreply.github.com>
2024-04-08 16:16:07 +0100

    added teams model

--- screens/models.py
+++ screens/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    designation = models.CharField(max_length = 30)
+    image = models.ImageField(upload_to ='media/')
+    email = models.EmailField(max_length=254)
+    phone_number = models.CharField(max_length = 15)
+
+    def __str__(self):
+        return self.first_name
