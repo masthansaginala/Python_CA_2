933551058758698d60fbc052712b1cc3896c539b
badmash417 <56015564+badmash417@users.noreply.github.com>
2024-04-14 14:07:24 +0100

    removed_merge_conflicst

--- screens/admin.py
+++ screens/admin.py
@@ -4,9 +4,5 @@ from .models import Team
 
 class TeamAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name", "designation")
-<<<<<<< HEAD
-
+    
 admin.site.register(Team, TeamAdmin)  
-=======
-admin.site.register(Team, TeamAdmin)
->>>>>>> 3aa4efe295bf39f5c63979f1840c6c1775476d0c
