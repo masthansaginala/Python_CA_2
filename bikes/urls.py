eb1d6c096ebdb5de7fa241eaad4d10d75819cd58
SARTHAK SINGHAL <sarthaksinghal@SARTHAKs-MacBook-Air.local>
2024-04-15 18:20:42 +0100

    added bike detail

--- bikes/urls.py
+++ bikes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BikeList
+from .views import BikeList, BikeDetail
 
 urlpatterns = [
     path('bikes/',BikeList.as_view(), name = 'bike-list'),
+    path('bikes/<int:pk>',BikeDetail.as_view(), name = 'bike-detail'),
 ]
