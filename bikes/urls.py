8d38a2ea9983d21318992de02f1ea0734984309b
Veeramalla26 <manasmanasa987@gmail.com>
2024-04-15 17:44:52 +0100

    added fautured list

--- bikes/urls.py
+++ bikes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BikeList
+from .views import BikeList, FeauturedList
 
 urlpatterns = [
     path('bikes/',BikeList.as_view(), name = 'bike-list'),
+    path('bikes/featured',FeauturedList.as_view(), name = 'feautured-list'),
 ]
--- bikes/views.py
+++ bikes/views.py
@@ -4,11 +4,21 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import BikeSerializer
 # Create your views here.
+#This Api is for Listing Bikes.
 class BikeList(APIView):
     def get(self,request):
         data = Bikes.objects.all()
         serializer = BikeSerializer(data, many = True)
         return Response(serializer.data)
+
+#This Api is for Listing Featured and Non Feautured Bike Lists.
+class FeauturedList(APIView):
+    def get(self, request):
+        category = request.GET.get('is_feautured')
+        data = Bikes.objects.filter(is_feautured = category)
+        serializer = BikeSerializer(data, many = True)
+        return Response(serializer.data)
+    
 
 
 
