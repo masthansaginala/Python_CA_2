108ff0af4c4aee544e045e324003a08bd86122c5
Veeramalla26 <158296490+Veeramalla26@users.noreply.github.com>
2024-04-16 14:13:16 +0100

    Update urls.py

--- bikes/urls.py
+++ bikes/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import BikeList, FeauturedList
-
-urlpatterns = [
-    path('bikes/',BikeList.as_view(), name = 'bike-list'),
-    path('bikes/featured',FeauturedList.as_view(), name = 'feautured-list'),
-=======
-from .views import BikeList, BikeDetail
+from .views import BikeList,BikeDetail,FeauturedList
 
 urlpatterns = [
     path('bikes/',BikeList.as_view(), name = 'bike-list'),
     path('bikes/<int:pk>',BikeDetail.as_view(), name = 'bike-detail'),
->>>>>>> 088fa85c2a355b4d2920dcb172c382d148cb068f
+    path('bikes/featured',FeauturedList.as_view(), name = 'feautured-list'),
+    
 ]
